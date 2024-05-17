8c28b3c66d6f56c0955dd8b8b53ffffdc7685ca4
MrG4ming <93336915+MrG4ming@users.noreply.github.com>
2024-05-17 15:14:13 +0200

    Big Rework

--- SoundCuer/MA3_SoundCuer.lua
+++ SoundCuer/MA3_SoundCuer.lua
@@ -1,41 +1,74 @@
-local function openMsgBox(sounds)
-	-- create inputs:
+-------------- Returns Numerrator for Numbering things (e.i. 0001 instead of 1) --------------
+local function getNumerator(i)
+	local numerator = tostring(i)
+	for a = 1, 4-string.len(tostring(i)) do
+		numerator = "0"..numerator
+	end
+	numerator = numerator..". "
+	
+	return numerator
+end
+
+-------------- First MsgBox: Choice of further Action
+local function openMsgBoxSelection(sounds)
 	local inputs = {
-		{name = "Store in Sequence", value = "1", whiteFilter = "0123456789"}
+		{name = "1. Store in Sequence", value = "1", whiteFilter = "0123456789"},
+		{name = "2. Sequence name"}
 	}
 
-	-- create states
 	local states = {
 		--{name = "State A", state = true, group = 1}
+		
 	}
-
-	for i = 1, #sounds:Children() do
---		Printf("Iteration: "..i)
-		
-		local sound = sounds:Children()[i]
-		
-		local soundName = sound["Name"]
-		local soundNumber = sound["No"]
-
-		table.insert(states, {name = soundName, state = true, group = soundNumber})
-		
---		Echo("Name: "..sound["Name"])
---		Echo("Number: "..sound["No"])
-		
---		Printf(" ")
---		Printf(cSound:Dump())
-	end
+	
+	local selectors = {
+        { name="Type of Selection", selectedValue=1, values={["All available"]=1,["Range"]=2,["Self"]=3}, type=1}
+    }
 
 
 	local msgOptions = {
-			title = "Please select sounds for Cue",
+			title = "Please select your configuration for the Selection",
 			commands = {{value = 0, name = "Cancel"}, {value = 1, name = "Ok"}},
 			states = states,
 			inputs = inputs,
 			selectors = selectors,
 			backColor = "Global.Default",
-			-- timeout = 10000, --milliseconds
-			-- timeoutResultCancel = false,
+			icon = "logo_small",
+			titleTextColor = "Global.Text",
+			messageTextColor = "Global.Text"
+		}
+		
+		
+	-- open messagebox:
+	local resultTable = MessageBox(msgOptions)
+	
+	return resultTable
+end
+
+-------------- All Sounds Selection --------------
+local function openMsgBoxAll(sounds)
+	local states = {
+		--{name = "State A", state = true, group = 1}	
+	}
+	
+	for i = 1, #sounds:Children() do
+		
+		local sound = sounds:Children()[i]
+		
+		local soundName = sound["Name"]
+		local soundNumber = sound["No"]
+				
+		table.insert(states, {name = getNumerator(i)..soundName, state = true, group = soundNumber})
+		
+	end
+
+	local msgOptions = {
+			title = "Customize your Selection",
+			commands = {{value = 0, name = "Cancel"}, {value = 1, name = "Ok"}},
+			states = states,
+			inputs = inputs,
+			selectors = selectors,
+			backColor = "Global.Default",
 			icon = "logo_small",
 			titleTextColor = "Global.Text",
 			messageTextColor = "Global.Text"
@@ -47,14 +80,119 @@ local function openMsgBox(sounds)
 	return resultTable
 end
 
+-------------- Range Sound Selection --------------
+local function openMsgBoxRange(sounds)
+	local inputs = {
+		{name = "1. Start", value = "1", whiteFilter = "0123456789"},
+		{name = "2. End", value = "2", whiteFilter = "0123456789"}
+	}
+	
+	local states = {
+		--{name = "State A", state = true, group = 1}	
+	}
 
-local function createSequence(seqNum, sounds, resultTable)
-	--Printf(tostring(resultTable.states[1]))
-	--Printf("Name of sequence 1: " .. DataPool().Sequences[seqNum].Name)
-	if DataPool().Sequences[seqNum] == nil then
-		Printf("Is nil")
-		Cmd("Store Sequence "..seqNum.." /CreateSecondCue")
+	local msgOptions = {
+			title = "Set Range",
+			commands = {{value = 0, name = "Cancel"}, {value = 1, name = "Ok"}},
+			states = states,
+			inputs = inputs,
+			selectors = selectors,
+			backColor = "Global.Default",
+			icon = "logo_small",
+			titleTextColor = "Global.Text",
+			messageTextColor = "Global.Text"
+		}
+		
+	-- open messagebox:
+	local rangeResultTable = MessageBox(msgOptions)
+		
+	---------- Second Part - Range Sound Selection ----------
+	
+	local states = {
+		--{name = "State A", state = true, group = 1}	
+	}
+	
+-- Add Sounds to Selection/States
+	for i = 1, #sounds:Children() do
+		local sound = sounds:Children()[i]
+		
+		local soundName = sound["Name"]
+		local soundNumber = sound["No"]
+		
+		if tonumber(rangeResultTable.inputs['1. Start']) <= i and i <= (tonumber(rangeResultTable.inputs['2. End'])) then
+			table.insert(states, {name = getNumerator(i)..soundName, state = true, group = soundNumber})
+		else
+			table.insert(states, {name = getNumerator(i)..soundName, state = false, group = soundNumber})
+		end
 	end
+
+
+	local msgOptions = {
+			title = "Customize your Selection",
+			commands = {{value = 0, name = "Cancel"}, {value = 1, name = "Ok"}},
+			states = states,
+			--inputs = inputs,
+			selectors = selectors,
+			backColor = "Global.Default",
+			icon = "logo_small",
+			titleTextColor = "Global.Text",
+			messageTextColor = "Global.Text"
+		}
+		
+	-- open messagebox:
+	local selectionResultTable = MessageBox(msgOptions)
+	
+	return selectionResultTable
+
+end
+
+local function openMsgBoxSelf(sounds)
+	local states = {
+		--{name = "State A", state = true, group = 1}	
+	}
+	
+	for i = 1, #sounds:Children() do
+		local sound = sounds:Children()[i]
+		
+		local soundName = sound["Name"]
+		local soundNumber = sound["No"]
+
+		table.insert(states, {name = getNumerator(i)..soundName, state = false, group = soundNumber})
+	end
+
+	local msgOptions = {
+			title = "Customize your Selection",
+			commands = {{value = 0, name = "Cancel"}, {value = 1, name = "Ok"}},
+			states = states,
+			inputs = inputs,
+			selectors = selectors,
+			backColor = "Global.Default",
+			icon = "logo_small",
+			titleTextColor = "Global.Text",
+			messageTextColor = "Global.Text"
+		}
+		
+	-- open messagebox:
+	local selectionResultTable = MessageBox(msgOptions)
+	
+	return selectionResultTable
+end
+
+
+-------------- Sequence Creator based on previous Selection --------------
+local function createSequence(seqNum, sounds, resultTable, seqName)
+
+	if DataPool().Sequences[seqNum] == nil then
+		Cmd("Store Sequence "..seqNum.." /CreateSecondCue")
+		Cmd("Label Sequence "..seqNum.." "..seqName)
+	else 
+		
+		Cmd("Delete Sequence "..seqNum)
+		Cmd("Store Sequence "..seqNum.." /CreateSecondCue")
+		Cmd("Label Sequence "..seqNum.." "..seqName)
+	end
+	
+	
 	Cmd("set seq "..seqNum.." cue \"OffCue\" property \"Command\" \"Off Sound 1 Thru\"")
 	--DataPool().Sequences:Insert(seqNum)
 	
@@ -66,23 +204,95 @@ local function createSequence(seqNum, sounds, resultTable)
 		
 		Printf("State: "..tostring(resultTable.states[sound["Name"]]))
 		
-		if tostring(state) then
+		if tostring(state) == "true" then
 			if DataPool().Sequences[seqNum][i+2] == nil then
 				Cmd("Store Sequence "..seqNum.." Cue "..i.." /CreateSecondCue")
-			end
+			end	
 			Cmd("set seq "..seqNum.." cue "..i.." property \"Command\" \"Off Sound 1 Thru; Go+ Sound "..sound["No"].."\"")
 			Cmd("set seq "..seqNum.." cue "..i.." property \"Name\" \""..sound["Name"]:gsub("%[",""):gsub("%]","").."\"")
 		end
 	end
+	
+	sound2 = sounds:Children()[1]
+	
+	if tostring(resultTable.states[sound2["Name"]]) == "false" then
+	Printf("check2")
+		Cmd("Delete Sequence "..seqNum.." Cue 1")
+	end
 end
 
+-------------- Sequence Override MsgBox when Sequence already exists --------------
+local function openMsgBoxOverride()
 
-local function main()
-	Cmd("clearall")
+	local msgOptions = {
+			title = "Attention: Sequence is not empty",
+			message = "The chosen Sequence is not empty. Would you like to override or change Sequence?",
+			commands = {{value = 2, name = "Cancel"},{value = 0, name = "Override"}, {value = 1, name = "Change Sequence"}},
+			states = states,
+			inputs = inputs,
+			selectors = selectors,
+			backColor = "Global.Default",
+			-- timeout = 10000, --milliseconds
+			-- timeoutResultCancel = false,
+			icon = "logo_small",
+			titleTextColor = "Global.Text",
+			messageTextColor = "Global.Text"
+		}
 	
-	local sounds = ShowData()["MediaPools"]["Sounds"]
+	local resultTable = MessageBox(msgOptions)
+	return resultTable
 	
-	-- start of dump
+end
+
+-------------- Parse Sound Choose Variant User Input --------------
+local function chooseSounds(resultTable, sounds)
+
+	if "1" == tostring(resultTable.selectors['Type of Selection']) then
+		
+		local selAll = openMsgBoxAll(sounds)
+		
+		if tonumber(selAll.result) == 0 then
+			return
+		end
+		
+		local seqNum = tonumber(resultTable.inputs['1. Store in Sequence'])
+		local seqName = "\""..tostring(resultTable.inputs['2. Sequence name']).."\""
+		
+		createSequence(seqNum, sounds, selAll, seqName)
+		
+	elseif tostring(resultTable.selectors['Type of Selection']) == "2" then
+			
+		local selRange = openMsgBoxRange(sounds)
+		
+		if tonumber(selRange.result) == 0 then
+			return
+		end
+		
+		local seqNum = tonumber(resultTable.inputs['1. Store in Sequence'])
+		local seqName = "\""..tostring(resultTable.inputs['2. Sequence name']).."\""		
+		
+		createSequence(seqNum, sounds, selRange, seqName)
+			
+	else 
+		
+		local selSelf = openMsgBoxSelf(sounds)
+		
+		if tonumber(selSelf.result) == 0 then
+			return
+		end
+
+		local seqNum = tonumber(resultTable.inputs['1. Store in Sequence'])
+		local seqName = "\""..tostring(resultTable.inputs['2. Sequence name']).."\""
+		Printf("Sequence Number: "..seqNum)
+	
+		createSequence(seqNum, sounds, selSelf, seqName)
+		
+	end 
+
+end
+
+local function debugDump()
+-------------- Start of Dump --------------
 	Printf("===============Start of Dump=================")
 	ShowData():Dump()
 	Printf("-----------------------")
@@ -103,34 +313,51 @@ local function main()
 	end
 	
 	Printf(" ")
-	Printf(" ")
-	Printf("-------------------")
+	--Printf(" ")
+	--Printf("-------------------")
 	--DataPool().Sequences[1]:Dump()
 	Printf("================End of Dump==================")
-	-- end of dump
+-------------- End of Dump --------------
+end
+
+local function main()
+	Cmd("clearall")
 	
+	--debugDump() --Only for Debugging
 	
-	local resultTable = openMsgBox(sounds)
+	local sounds = ShowData()["MediaPools"]["Sounds"]
+		
+	::changeSequence::
+	local resultTable = openMsgBoxSelection(sounds)
 	
-	-- print results:
-	Printf("Success = "..tostring(resultTable.success))
-	--exit on Cancel:
-	Printf("Result = "..resultTable.result)
 	if tonumber(resultTable.result) == 0 then
 		return
 	end
-	--Printf("States = "..tostring(resultTable.states[1]))
 	
---	for k,v in pairs(resultTable.inputs) do
---        Printf("Input '%s' = '%s'",k,v)
---    end
+	if DataPool().Sequences[tonumber(resultTable.inputs['1. Store in Sequence'])] == nil then
+		
+		chooseSounds(resultTable, sounds)
+		return main
+		
+	else
+		-------------- Manage Sequence Override --------------
+		local msgOverride = openMsgBoxOverride()
+		
+		if tonumber(msgOverride.result) == 2 then
+			return
+		end
+		
+		if tonumber(msgOverride.result) == 1 then
+			goto changeSequence
+		end
+		
+		if tonumber(msgOverride.result) == 0 then
+			chooseSounds(resultTable, sounds)
+		end
+		
+	end
 	
-	--local seqNum = 1
-	local seqNum = tonumber(resultTable.inputs['Store in Sequence'])
-	Printf("Sequence Number: "..seqNum)
-	
-	createSequence(seqNum, sounds, resultTable)
-	
+	Printf("Success!")
 end
 
 return main
